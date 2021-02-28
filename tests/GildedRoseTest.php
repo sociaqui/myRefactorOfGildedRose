96ebf928aec8707adc394e0bb20dd84fb57816a9
Jarek <sociaqui@gmail.com>
2021-02-28 12:20:28 +0100

    separated the tests

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -11,105 +11,17 @@ use PHPUnit\Framework\TestCase;
 class GildedRoseTest extends TestCase
 {
     /**
-     * @covers       GildedRose\Item
-     * @param array  $input
-     * @param string $expected
-     * @uses         GildedRose\Item
-     *
-     * @dataProvider itemProvider
-     *
-     */
-    public function testItemCreation(array $input, string $expected): void
-    {
-        $item = new Item($input['name'], $input['sellIn'], $input['quality']);
-        $this->assertSame($expected, (string)$item);
-    }
-
-    /**
-     * @depends      testItemCreation
-     *
      * @covers       GildedRose\GildedRose::updateQuality()
      * @param Item   $item
      * @param Item   $expected
-     * @uses         GildedRose\GildedRose::updateQuality()
      *
      * @dataProvider updateQualityProvider
-     *
      */
     public function testUpdateQuality(Item $item, Item $expected): void
     {
         $gildedRose = new GildedRose([$item]);
         $gildedRose->updateQuality();
         $this->assertSame((string)$expected, (string)$item);
-    }
-
-    public function itemProvider(): array
-    {
-        return [
-            [
-                'item' => [
-                    'name' => '+7 Agility Vambrace',
-                    'sellIn' => 5,
-                    'quality' => 10,
-                ],
-                'expected' => '+7 Agility Vambrace, 5, 10'
-            ],
-            [
-                'item' => [
-                    'name' => '+3 Defense Shield',
-                    'sellIn' => 0,
-                    'quality' => 7,
-                ],
-                'expected' => '+3 Defense Shield, 0, 7'
-            ],
-            [
-                'item' => [
-                    'name' => 'Backstage passes to a TAFKAL80ETC concert',
-                    'sellIn' => 4,
-                    'quality' => 0,
-                ],
-                'expected' => 'Backstage passes to a TAFKAL80ETC concert, 4, 0'
-            ],
-            // legendary Item - only one allowed to have a quality of 80 (over 50)
-            [
-                'item' => [
-                    'name' => 'Sulfuras, Hand of Ragnaros',
-                    'sellIn' => 0,
-                    'quality' => 80,
-                ],
-                'expected' => 'Sulfuras, Hand of Ragnaros, 0, 80'
-            ],
-            // is it even OK to create new Items already after the Sell In date?
-            [
-                'item' => [
-                    'name' => 'Conjured Mana Muffin',
-                    'sellIn' => -2,
-                    'quality' => 5,
-                ],
-                'expected' => 'Conjured Mana Muffin, -2, 5'
-            ],
-            /** what about creating new Items wth stats already outside of the rules:
-             * The Quality of an item is never negative
-             * The Quality of an item is never more than 50
-             * TODO: block the possibility to create such items somehow
-             * [
-             * 'item' => [
-             * 'name' => 'cheap knockoff of Sulfuras, not legendary',
-             * 'sellIn' => 5,
-             * 'quality' => 77,
-             * ],
-             * 'expected' => 'some error during creation'
-             * ],
-             * [
-             * 'item' => [
-             * 'name' => 'useless junk',
-             * 'sellIn' => 50,
-             * 'quality' => -10,
-             * ],
-             * 'expected' => 'some error during creation'
-             * ],
-             */
-        ];
     }
 
     public function updateQualityProvider(): array
---
+++ tests/ItemTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use GildedRose\Item;
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase
+{
+    /**
+     * @covers       GildedRose\Item
+     * @param array  $input
+     * @param string $expected
+     *
+     * @dataProvider itemProvider
+     */
+    public function testItemCreation(array $input, string $expected): void
+    {
+        $item = new Item($input['name'], $input['sellIn'], $input['quality']);
+        $this->assertSame($expected, (string)$item);
+    }
+
+    public function itemProvider(): array
+    {
+        return [
+            [
+                'item' => [
+                    'name' => '+7 Agility Vambrace',
+                    'sellIn' => 5,
+                    'quality' => 10,
+                ],
+                'expected' => '+7 Agility Vambrace, 5, 10'
+            ],
+            [
+                'item' => [
+                    'name' => '+3 Defense Shield',
+                    'sellIn' => 0,
+                    'quality' => 7,
+                ],
+                'expected' => '+3 Defense Shield, 0, 7'
+            ],
+            [
+                'item' => [
+                    'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                    'sellIn' => 4,
+                    'quality' => 0,
+                ],
+                'expected' => 'Backstage passes to a TAFKAL80ETC concert, 4, 0'
+            ],
+            // legendary Item - only one allowed to have a quality of 80 (over 50)
+            [
+                'item' => [
+                    'name' => 'Sulfuras, Hand of Ragnaros',
+                    'sellIn' => 0,
+                    'quality' => 80,
+                ],
+                'expected' => 'Sulfuras, Hand of Ragnaros, 0, 80'
+            ],
+            // is it even OK to create new Items already after the Sell In date?
+            [
+                'item' => [
+                    'name' => 'Conjured Mana Muffin',
+                    'sellIn' => -2,
+                    'quality' => 5,
+                ],
+                'expected' => 'Conjured Mana Muffin, -2, 5'
+            ],
+            /** what about creating new Items wth stats already outside of the rules:
+             * The Quality of an item is never negative
+             * The Quality of an item is never more than 50
+             * TODO: block the possibility to create such items somehow
+             * [
+             * 'item' => [
+             * 'name' => 'cheap knockoff of Sulfuras, not legendary',
+             * 'sellIn' => 5,
+             * 'quality' => 77,
+             * ],
+             * 'expected' => 'some error during creation'
+             * ],
+             * [
+             * 'item' => [
+             * 'name' => 'useless junk',
+             * 'sellIn' => 50,
+             * 'quality' => -10,
+             * ],
+             * 'expected' => 'some error during creation'
+             * ],
+             */
+        ];
+    }
+}
